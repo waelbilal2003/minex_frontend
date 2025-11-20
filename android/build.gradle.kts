00ff1854e5140db2b3d79ce5980ec3dfb7b0b90d
waelbilal2003 <waelblal15@gmail.com>
2025-11-20 22:20:17 +0300

    وحل مشكلة البناء

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,25 +1,3 @@
-buildscript {
-    // ⭐ ⭐ ⭐ الطريقة الصحيحة لتعريف المتغير ⭐ ⭐ ⭐
-    // 1. تعريف كـ `ext` property
-    // ext.kotlin_version = "2.1.0"
-
-    // أو 2. تعريف كـ `extra` property (الطريقة المفضلة في Kotlin DSL)
-    // extra["kotlin_version"] = "2.1.0" // ⭐ ⭐ ⭐ هذا هو السطر المهم ⭐ ⭐ ⭐
-
-    // أو 3. تعريف كـ متغير محلي داخل block
-    val kotlin_version = "2.1.0" // ⭐ ⭐ ⭐ هذا هو السطر المهم ⭐ ⭐ ⭐
-
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.9.1")
-        // ⭐ ⭐ ⭐ استخدام المتغير بعد تعريفه ⭐ ⭐ ⭐
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version") // ⭐ ⭐ ⭐ الآن $kotlin_version معرف ⭐ ⭐ ⭐
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -27,7 +5,10 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
@@ -41,13 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-// ⭐ ⭐ ⭐ تأكد أن قسم plugins هذا في الملف الخاطئ ⭐ ⭐ ⭐
-// ⭐ ⭐ ⭐ يجب أن يكون في android/app/build.gradle.kts ⭐ ⭐ ⭐
-//plugins {
-//    id("com.android.application")
-//    id("kotlin-android")
-//    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-//    id("dev.flutter.flutter-gradle-plugin")
-//    id("com.google.gms.google-services")
-//}
